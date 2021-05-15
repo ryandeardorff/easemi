956d341e3ba131f185d1b2c8287058e77f6fb489
RyanDeardorff <ryansdeardorff@gmail.com>
2021-05-14 18:27:54 -0700

    Revert "Testing new git workflow"
    This reverts commit a85c382af050bcc0ff8f12dee43bdb67da56688d.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Canvas from "./core/Canvas.svelte"; //this is a test comment
+  import Canvas from "./core/Canvas.svelte";
 </script>
 
 <main on:contextmenu|preventDefault on:drag|preventDefault on:dragstart|preventDefault on:dragenter|preventDefault>
